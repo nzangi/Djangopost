80edeefcb57aac583d2d162c66f3029525bd612c
nzangi <nzangimuoki284@gmail.com>
2023-09-21 17:45:12 +0300

    Update,Delete and create Forms added

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from PIL import Image
 from django.contrib.auth.models import User
 
 
@@ -7,8 +7,19 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg',upload_to='profile_pics')
+    image = models.ImageField(default='default.png',upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile' #model yake
+    
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300or img.width >200:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
 
